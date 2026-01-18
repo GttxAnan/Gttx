92c959d8f3975601156fd899713e1daee3f79c46
Anandhu Anil <54853580+Anandhuka@users.noreply.github.com>
2026-01-19 00:26:13 +0530

    config file for vercel

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+// Default configuration
+// This file can be overwritten during deployment to inject environment variables
+window.BACKEND_URL = "http://localhost:5000"; // Default for local development
